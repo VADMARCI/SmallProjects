d76c350ccf79417559d4c97a09d76f08886f64a3
Marton Vadasz <75419452+VADMARCI@users.noreply.github.com>
2021-10-09 14:07:15 +0200

    Summing with recursion

---
+++ RecursionSumming/RecursionSumming/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursionSumming
+{
+    class Program
+    {
+        static int SumOfTen(int min, int max)
+        {
+            return CalcuSum(min, max);
+        }
+
+        static int CalcuSum(int min, int val)
+        {
+            if (val == min)
+            {
+                return val;
+            }
+            return val + CalcuSum(min, val - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number n you want to get the sum of");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum is: {0}", SumOfTen(1,y));
+        }
+    }
+}
